d731361114048a4dcfa833ea3daf12c024f869f9
Görkem Paradise <smailgorkem@gmail.com>
2026-08-24 14:41:56 +0300

    gcc repo

---
+++ scratch/00_hello.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+/*
+no using namespace std; 
+then you have to use std::cout, std::endl, etc. 
+
+std::cout << "Hello, World!" << std::endl;
+
+*/
+
+//  \n = endl 
+
+int main() {
+    cout << "Hello, World! \n";
+
+    int a = 5;
+    int b = 10;
+    cout << "The sum of " << a << " and " << b << " is " << (a + b) << endl;
+
+    return 0;
+}
+
---
+++ scratch/01_variable.cpp
@@ -0,0 +1,14 @@
+#include "iostream"
+
+int main() {
+
+    int myNum = 5;               // Integer (whole number without decimals)
+    double myFloatNum = 5.99;    // Floating point number (with decimals)
+    char myLetter = 'D';         // Character
+    bool myBoolean = true;       // Boolean (true or false)
+
+    char myString[] = "Hello";     // String (text)
+    char myArray[] = {'H', 'e', 'l', 'l', 'o'}; // Character array (text)
+    int myArray2[] = {1, 2, 3, 4, 5}; // Integer array
+    
+}
